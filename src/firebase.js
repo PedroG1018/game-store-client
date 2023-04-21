1f27fec6dc2b2887783a0279282bfa618fed2ee6
Pedro Gutierrez Jr <gutierrezpedro2112@gmail.com>
2023-04-21 13:05:54 -0500

    tested ability to add new products to the store

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
@@ -16,6 +16,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 export const auth = getAuth();
+export const storage = getStorage(app);
--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,11 +1,44 @@
-import React from "react";
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  setDoc,
+} from "firebase/firestore";
+import { db, storage } from "../firebase";
+import React, { useState } from "react";
 
 const New = () => {
+  const [file, setFile] = useState("");
+
+  const handleAdd = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await addDoc(collection(db, "products"), {
+        condition: "New",
+        desc: "test product",
+        details: {
+          platform: "test",
+          region: "test",
+          releaseDate: serverTimestamp(),
+          type: "test",
+        },
+        image: "test",
+        name: "test",
+        price: 100.99,
+        quantity: 4,
+      });
+      console.log(res.id);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="lg:flex">
-      <form>
-        <div></div>
-        <div></div>
+      <form onSubmit={handleAdd}>
+        <button type="submit">add</button>
       </form>
     </div>
   );
