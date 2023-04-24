31551b21a4ea5ef51c014c92b4d3b21eff67a329
Pedro Gutierrez Jr <gutierrezpedro2112@gmail.com>
2023-04-24 11:46:39 -0500

    able to add new products and have them stored to firestore

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -78,7 +78,7 @@ const New = () => {
 
     try {
       const res = await addDoc(collection(db, "products"), data);
-      console.log(res.id);
+      alert(res.id);
     } catch (error) {
       console.log(error);
     }
@@ -113,7 +113,12 @@ const New = () => {
         <div className="space-x-10 flex justify-center">
           <div>
             <label className="font-semibold">Product Name:</label>
-            <input type="text" className="border ml-2 pl-2" id="name" />
+            <input
+              type="text"
+              className="border ml-2 pl-2"
+              id="name"
+              onChange={handleInput}
+            />
           </div>
           <div>
             <label className="font-semibold">Price:</label>
@@ -121,6 +126,7 @@ const New = () => {
               type="number"
               className="border ml-2 pl-2 w-[6em]"
               id="price"
+              onChange={handleInput}
             />
           </div>
           <div>
@@ -129,6 +135,7 @@ const New = () => {
               type="number"
               className="border ml-2 pl-2 w-[6em] mr-2"
               id="quantity"
+              onChange={handleInput}
             />
           </div>
         </div>
@@ -194,7 +201,7 @@ const New = () => {
         </div>
 
         <div className="mt-4">
-          <h3 className="font-semibold text-left">Condition:</h3>
+          <h3 className="font-semibold text-left">Type:</h3>
           <ul className=" w-[40em] text-sm font-medium border border-gray-200 rounded-lg sm:flex">
             <li className="w-full border-b border-gray-200 sm:border-b-0 sm:borer-r-0">
               <div className="flex items-center pl-3">
@@ -240,6 +247,8 @@ const New = () => {
           <input
             type="text"
             className="border border-gray-300 focus:ring-blue-500 focus:ring-2 rounded w-full h-[6em]"
+            id="desc"
+            onChange={handleInput}
           />
         </div>
 
