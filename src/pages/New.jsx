05cc785113f4bfbc55ba2a61332804e770f40ce5
Pedro Gutierrez Jr <gutierrezpedro2112@gmail.com>
2023-04-25 13:45:18 -0500

    adding new products works now

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -17,14 +17,12 @@ import {
   Input,
   Typography,
   Button,
-  Checkbox,
   Textarea,
   Radio,
 } from "@material-tailwind/react";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { Label } from "@mui/icons-material";
 
 const New = () => {
   const [file, setFile] = useState("");
@@ -67,6 +65,8 @@ const New = () => {
         }
       );
     };
+
+    // run upload file function if a file has been selected
     file && uploadFile();
   }, [file]);
 
@@ -75,14 +75,18 @@ const New = () => {
     const id = e.target.id;
     const value = e.target.value;
 
+    console.log(id + ": " + value);
+
     setData({ ...data, [id]: value });
   };
 
   const handleRadio = (e) => {
     const name = e.target.name;
-    const value = e.target.value;
+    const id = e.target.id;
 
-    setData({ ...data, [name]: value });
+    console.log(name + ": " + id);
+
+    setData({ ...data, [name]: id });
   };
 
   // changes
@@ -106,61 +110,93 @@ const New = () => {
       <Typography variant="h3" className="text-blue-700">
         NEW PRODUCT
       </Typography>
-      <form
-        className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96"
-        onSubmit={handleAdd}
-      >
-        <div className="mb-4 flex flex-col gap-6 mx-auto justify-center">
-          <Input size="lg" label="Product Name" />
-          <Input size="lg" label="Price ($X.XX)" />
-          <Input type="number" size="lg" label="Quantity" />
-        </div>
-        <div className="flex gap-10 justify-center">
-          <Radio id="new" name="condition" label="New" />
-          <Radio id="refurbished" name="condition" label="Refurbished" />
-          <Radio id="used" name="condition" label="Used" />
-        </div>
-        <div className="flex gap-10 justify-center mb-4">
-          <Radio id="console" name="type" label="Console" />
-          <Radio id="game" name="type" label="Game" />
-        </div>
-        <div className="mb-6 flex flex-col gap-6">
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker
-              label="Release Date"
-              slotProps={{ textField: { helperText: "MM/DD/YYYY" } }}
+      <form className="mt-8 mb-2 lg:flex lg:flex-row justify-center gap-10 lg:w-full">
+        <div>
+          <div className="mb-4 flex flex-col gap-6">
+            <Input
+              size="lg"
+              label="Product Name"
+              onChange={handleInput}
+              id="name"
             />
-          </LocalizationProvider>
-          <Select label="Select Platform">
-            <Option>N64</Option>
-            <Option>N64</Option>
-            <Option>N64</Option>
-            <Option>N64</Option>
-            <Option>N64</Option>
-          </Select>
-        </div>
-        <div className="">
-          <Textarea label="Description" />
-        </div>
-        <Checkbox
-          label={
-            <Typography
-              variant="small"
-              color="gray"
-              className="flex items-center font-normal"
+            <Input size="lg" label="Price" onChange={handleInput} id="price" />
+            <Input
+              type="number"
+              size="lg"
+              label="Quantity"
+              onChange={handleInput}
+              id="quantity"
+            />
+          </div>
+          <div>
+            <Typography className="ml-3 font-semibold">Condition</Typography>
+            <Radio
+              id="new"
+              name="condition"
+              label="New"
+              onChange={handleRadio}
+            />
+            <Radio
+              id="refurbished"
+              name="condition"
+              label="Refurbished"
+              onChange={handleRadio}
+            />
+            <Radio
+              id="used"
+              name="condition"
+              label="Used"
+              onChange={handleRadio}
+            />
+          </div>
+          <div className="mb-4">
+            <Typography className="ml-3 font-semibold">Type</Typography>
+            <Radio
+              id="console"
+              name="type"
+              label="Console"
+              onChange={handleRadio}
+            />
+            <Radio id="game" name="type" label="Game" onChange={handleRadio} />
+          </div>
+          <div className="mb-6 flex flex-col gap-6">
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                label="Release Date"
+                slotProps={{ textField: { helperText: "MM/DD/YYYY" } }}
+                onChange={(newValue) => {
+                  const releaseDate = newValue.$d.toLocaleDateString();
+                  setData({ ...data, releaseDate: releaseDate });
+                }}
+              />
+            </LocalizationProvider>
+            <Select
+              label="Select Platform"
+              id="platform"
+              onChange={(newValue) => {
+                setData({ ...data, platform: newValue });
+              }}
             >
-              I agree the
-              <a
-                href="#"
-                className="font-medium transition-colors hover:text-blue-500"
-              >
-                &nbsp;Terms and Conditions
-              </a>
-            </Typography>
-          }
-          containerProps={{ className: "-ml-2.5" }}
-        />
-        <div className="w-[20em] mb-10 mx-auto">
+              <Option value="NES">NES</Option>
+              <Option value="SNES">SNES</Option>
+              <Option value="N64">N64</Option>
+              <Option value="Gamecube">Gamecube</Option>
+              <Option value="Wii">Wii</Option>
+            </Select>
+            <Select
+              label="Select Region"
+              id="region"
+              onChange={(newValue) => {
+                setData({ ...data, region: newValue });
+              }}
+            >
+              <Option value="NTSC (US, CANADA)">NTSC (US, CANADA)</Option>
+              <Option value="NTSJ (JAPAN)">NTSJ (JAPAN)</Option>
+            </Select>
+          </div>
+          <Textarea label="Description" id="desc" onChange={handleInput} />
+        </div>
+        <div className="max-w-sm items-center mt-4 lg:mt-0">
           <img
             src={
               file
@@ -168,9 +204,9 @@ const New = () => {
                 : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
             }
             alt="product"
-            className="object-center mb-2"
+            className="mb-2"
           />
-          <div className="text-center">
+          <div className="text-center block">
             <label htmlFor="file" className="font-semibold">
               Upload Image <DriveFolderUploadOutlinedIcon />
             </label>
@@ -182,10 +218,10 @@ const New = () => {
             />
           </div>
         </div>
-        <Button className="mt-6" fullWidth>
-          Add
-        </Button>
       </form>
+      <Button className="mt-6 w-60" onClick={handleAdd}>
+        Add
+      </Button>
     </Card>
   );
 };
