b6b6e70276cba898896ee5663dca91b89e50db8d
Pedro Gutierrez Jr <gutierrezpedro2112@gmail.com>
2023-04-25 11:06:56 -0500

    redesigned new product page

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
+import { ThemeProvider } from "@material-tailwind/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
+    <ThemeProvider>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -18,7 +18,13 @@ import {
   Typography,
   Button,
   Checkbox,
+  Textarea,
+  Radio,
 } from "@material-tailwind/react";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { Label } from "@mui/icons-material";
 
 const New = () => {
   const [file, setFile] = useState("");
@@ -76,8 +82,6 @@ const New = () => {
     const name = e.target.name;
     const value = e.target.value;
 
-    console.log(value);
-
     setData({ ...data, [name]: value });
   };
 
@@ -94,271 +98,95 @@ const New = () => {
   };
 
   return (
-    <>
-      <Card color="transparent" shadow={false}>
-        <Typography variant="h4" color="blue-gray">
-          Sign Up
-        </Typography>
-        <Typography color="gray" className="mt-1 font-normal">
-          Enter your details to register.
-        </Typography>
-        <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
-          <div className="mb-4 flex flex-col gap-6">
-            <Input size="lg" placeholder="Name" />
-            <Input size="lg" label="Email" />
-            <Input type="password" size="lg" label="Password" />
-          </div>
-          <Checkbox
-            label={
-              <Typography
-                variant="small"
-                color="gray"
-                className="flex items-center font-normal"
-              >
-                I agree the
-                <a
-                  href="#"
-                  className="font-medium transition-colors hover:text-blue-500"
-                >
-                  &nbsp;Terms and Conditions
-                </a>
-              </Typography>
-            }
-            containerProps={{ className: "-ml-2.5" }}
-          />
-          <Button className="mt-6" fullWidth>
-            Register
-          </Button>
-          <Typography color="gray" className="mt-4 text-center font-normal">
-            Already have an account?{" "}
-            <a
-              href="#"
-              className="font-medium text-blue-500 transition-colors hover:text-blue-700"
-            >
-              Sign In
-            </a>
-          </Typography>
-        </form>
-      </Card>
-      <div className="my-10 mx-20">
-        <h1 className="text-blue-700 font-semibold text-3xl pb-4">
-          NEW PRODUCT
-        </h1>
-        <Card color="transparent">
-          <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
-            <div className="mb-4 flex flex-col gap-6">
-              <Input size="lg" label="Name" />
-              <Input size="lg" label="Email" />
-            </div>
-          </form>
-        </Card>
-        <form onSubmit={handleAdd} className=" block m-0 max-w-screen-md">
-          <div className="w-[20em] mb-10 mx-auto">
-            <img
-              src={
-                file
-                  ? URL.createObjectURL(file)
-                  : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
-              }
-              alt="product"
-              className="object-center mb-2"
+    <Card
+      color="transparent"
+      shadow={false}
+      className="mx-auto my-10 items-center"
+    >
+      <Typography variant="h3" className="text-blue-700">
+        NEW PRODUCT
+      </Typography>
+      <form
+        className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96"
+        onSubmit={handleAdd}
+      >
+        <div className="mb-4 flex flex-col gap-6 mx-auto justify-center">
+          <Input size="lg" label="Product Name" />
+          <Input size="lg" label="Price ($X.XX)" />
+          <Input type="number" size="lg" label="Quantity" />
+        </div>
+        <div className="flex gap-10 justify-center">
+          <Radio id="new" name="condition" label="New" />
+          <Radio id="refurbished" name="condition" label="Refurbished" />
+          <Radio id="used" name="condition" label="Used" />
+        </div>
+        <div className="flex gap-10 justify-center mb-4">
+          <Radio id="console" name="type" label="Console" />
+          <Radio id="game" name="type" label="Game" />
+        </div>
+        <div className="mb-6 flex flex-col gap-6">
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              label="Release Date"
+              slotProps={{ textField: { helperText: "MM/DD/YYYY" } }}
             />
-            <div className="text-center">
-              <label htmlFor="file" className="font-semibold">
-                Upload Image <DriveFolderUploadOutlinedIcon />
-              </label>
-              <input
-                type="file"
-                id="file"
-                onChange={(e) => setFile(e.target.files[0])}
-                style={{ display: "none" }}
-              />
-            </div>
-          </div>
-
-          <div className="justify-left">
-            <div className="space-x-10 flex">
-              <div>
-                <label className="block font-semibold">Product Name</label>
-                <input
-                  type="text"
-                  className="border py-1 px-2"
-                  id="name"
-                  onChange={handleInput}
-                  placeholder="name"
-                />
-              </div>
-              <div>
-                <label className="block font-semibold">Price</label>
-                <input
-                  type="decimal"
-                  className="border py-1 px-2 w-[6em]"
-                  id="price"
-                  onChange={handleInput}
-                  placeholder="$"
-                />
-              </div>
-              <div>
-                <label className="block font-semibold">Quantity</label>
-                <input
-                  type="number"
-                  className="border py-1 px-2 w-[6em]"
-                  id="quantity"
-                  onChange={handleInput}
-                  placeholder="#"
-                />
-              </div>
-            </div>
-
-            <div className="mt-4 items-center justify-center flex">
-              <ul className=" w-[40em] text-sm font-medium border border-gray-200 rounded-lg md:flex">
-                <li className="w-full border-b border-gray-200 md:border-r-0">
-                  <div className="flex items-center pl-3">
-                    <input
-                      type="radio"
-                      value="New"
-                      name="condition"
-                      id="list-radio-new"
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2"
-                      onChange={handleRadio}
-                    />
-                    <label
-                      htmlFor="list-radio-new"
-                      className="w-full py-3 ml-2 text-sm font-medium "
-                    >
-                      New
-                    </label>
-                  </div>
-                </li>
-                <li className="w-full border-b border-gray-200 md:border-b-0">
-                  <div className="flex items-center pl-3">
-                    <input
-                      type="radio"
-                      value="Refurbished"
-                      name="condition"
-                      id="list-radio-refurbished"
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2"
-                      onChange={handleRadio}
-                    />
-                    <label
-                      htmlFor="list-radio-refurbished"
-                      className="w-full py-3 ml-2 text-sm font-medium"
-                    >
-                      Refurbished
-                    </label>
-                  </div>
-                </li>
-                <li className="w-full border-b border-gray-200 sm:border-b-0 sm:borer-r-0">
-                  <div className="flex items-center pl-3">
-                    <input
-                      type="radio"
-                      value="Used"
-                      name="condition"
-                      id="list-radio-used"
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2"
-                      onChange={handleRadio}
-                    />
-                    <label
-                      htmlFor="list-radio-used"
-                      className="w-full py-3 ml-2 text-sm font-medium "
-                    >
-                      Used
-                    </label>
-                  </div>
-                </li>
-              </ul>
-            </div>
-
-            <div className="mt-4 flex justify-center">
-              <ul className=" w-[40em] text-sm font-medium border border-gray-200 rounded-lg sm:flex">
-                <li className="w-full border-b border-gray-200 sm:border-b-0 sm:borer-r-0">
-                  <div className="flex items-center pl-3">
-                    <input
-                      type="radio"
-                      value="Console"
-                      name="type"
-                      id="list-radio-console"
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2"
-                      onChange={handleRadio}
-                    />
-                    <label
-                      htmlFor="list-radio-console"
-                      className="w-full py-3 ml-2 text-sm font-medium "
-                    >
-                      Console
-                    </label>
-                  </div>
-                </li>
-                <li className="w-full border-b border-gray-200 sm:border-b-0 sm:borer-r-0">
-                  <div className="flex items-center pl-3">
-                    <input
-                      type="radio"
-                      value="Game"
-                      name="type"
-                      id="list-radio-game"
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2"
-                      onChange={handleRadio}
-                    />
-                    <label
-                      htmlFor="list-radio-game"
-                      className="w-full py-3 ml-2 text-sm font-medium"
-                    >
-                      Game
-                    </label>
-                  </div>
-                </li>
-              </ul>
-            </div>
-
-            <div className="mt-4 flex">
-              <div>
-                <label htmlFor="date" className="block font-semibold">
-                  Release Date
-                </label>
-                <input
-                  type="date"
-                  className="bg-gray-50 border border-gray-300 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 pl-5 p-2.5"
-                />
-              </div>
-              <div>
-                <label htmlFor="platform" className="block font-semibold">
-                  Platform
-                </label>
-                <div className="w-72">
-                  <Select label="Select Platform" className=" h-full">
-                    <Option>N64</Option>
-                    <Option>N64</Option>
-                    <Option>N64</Option>
-                    <Option>N64</Option>
-                    <Option>N64</Option>
-                  </Select>
-                </div>
-              </div>
-            </div>
-
-            <div className="mt-4 flex justify-center">
-              <div className="w-[30em]">
-                <label className="font-semibold block">Description</label>
-                <textarea
-                  className="border border-gray-300 focus:ring-blue-500 focus:ring-2 rounded h-20 py-1 px-2 w-[30em] bg-gray-50"
-                  id="desc"
-                  onChange={handleInput}
-                />
-              </div>
-            </div>
-
-            <div className="text-right">
-              <button
-                type="submit"
-                className="mt-4 bg-blue-700 rounded-lg text-white py-3 px-8 focus:ring-blue-500 focus:border-blue-500 hover:bg-red-600 font-bold"
+          </LocalizationProvider>
+          <Select label="Select Platform">
+            <Option>N64</Option>
+            <Option>N64</Option>
+            <Option>N64</Option>
+            <Option>N64</Option>
+            <Option>N64</Option>
+          </Select>
+        </div>
+        <div className="">
+          <Textarea label="Description" />
+        </div>
+        <Checkbox
+          label={
+            <Typography
+              variant="small"
+              color="gray"
+              className="flex items-center font-normal"
+            >
+              I agree the
+              <a
+                href="#"
+                className="font-medium transition-colors hover:text-blue-500"
               >
-                Add
-              </button>
-            </div>
+                &nbsp;Terms and Conditions
+              </a>
+            </Typography>
+          }
+          containerProps={{ className: "-ml-2.5" }}
+        />
+        <div className="w-[20em] mb-10 mx-auto">
+          <img
+            src={
+              file
+                ? URL.createObjectURL(file)
+                : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+            }
+            alt="product"
+            className="object-center mb-2"
+          />
+          <div className="text-center">
+            <label htmlFor="file" className="font-semibold">
+              Upload Image <DriveFolderUploadOutlinedIcon />
+            </label>
+            <input
+              type="file"
+              id="file"
+              onChange={(e) => setFile(e.target.files[0])}
+              style={{ display: "none" }}
+            />
           </div>
-        </form>
-      </div>
-    </>
+        </div>
+        <Button className="mt-6" fullWidth>
+          Add
+        </Button>
+      </form>
+    </Card>
   );
 };
 
