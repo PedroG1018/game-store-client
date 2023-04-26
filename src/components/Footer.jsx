35d71ac39dc88a2cec55327e1a1fc8e2ff0b1627
Pedro Gutierrez Jr <gutierrezpedro2112@gmail.com>
2023-04-26 18:07:58 -0500

    redesigned footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,50 +1,63 @@
 import React, { useState } from "react";
 import { AiOutlineSend } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import { Typography } from "@material-tailwind/react";
+import icon from "../../src/img/1up.png";
 
 const Footer = () => {
   return (
-    <footer className="text-white">
-      <div className="bg-blue-900 text-center py-4 m-auto">
-        <h1 className="pb-3 text-2xl font-semibold">Contact</h1>
-        <div className="items-center space-x-2">
-          <input
-            type="email"
-            placeholder="Enter your email"
-            className="p-2 rounded-lg"
-          />
-          <button type="button" className="bg-gray-500 p-3">
-            <AiOutlineSend />
-          </button>
-        </div>
+    <footer className="w-full bg-blue-800 p-8 mt-10">
+      <div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 text-center md:justify-between text-white">
+        <a href="/">
+          <img src={icon} alt="logo-ct" className="w-10" />
+        </a>
+        <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
+          <li>
+            <Typography
+              as="a"
+              href="#"
+              color="blue-gray"
+              className="font-normal transition-colors hover:text-white focus:text-white text-gray-200"
+            >
+              About Us
+            </Typography>
+          </li>
+          <li>
+            <Typography
+              as="a"
+              href="#"
+              color="blue-gray"
+              className="font-normal transition-colors hover:text-white focus:text-white text-gray-200"
+            >
+              Shopping Cart
+            </Typography>
+          </li>
+          <li>
+            <Typography
+              as="a"
+              href="#"
+              color="blue-gray"
+              className="font-normal transition-colors hover:text-white focus:text-white text-gray-200"
+            >
+              Orders
+            </Typography>
+          </li>
+          <li>
+            <Typography
+              as="a"
+              href="#"
+              color="blue-gray"
+              className="font-normal transition-colors hover:text-white focus:text-white text-gray-200"
+            >
+              Contact Us
+            </Typography>
+          </li>
+        </ul>
       </div>
-      <hr />
-      <div className="bg-blue-700 py-2">
-        <div className="grid grid-cols-2 justify-items-center py-10">
-          <div>
-            <h1 className="font-bold text-xl border-b-4 border-yellow-400 mb-2 pb-2">
-              1-UP STORE
-            </h1>
-            <ul>
-              <li>About Us</li>
-              <li>Sell Video Games</li>
-              <li>Repair Center</li>
-            </ul>
-          </div>
-          <div>
-            <h1 className="font-bold text-xl border-b-4 border-yellow-400 mb-2 pb-2">
-              ACCOUNT
-            </h1>
-            <ul>
-              <li>
-                <Link to="/">My Account</Link>
-              </li>
-              <li>Shopping Cart</li>
-              <li>Accessibility</li>
-            </ul>
-          </div>
-        </div>
-      </div>
+      <hr className="my-8 border-blue-gray-50" />
+      <Typography color="white" className="text-center font-normal">
+        &copy; 2023 Material Tailwind
+      </Typography>
     </footer>
   );
 };
