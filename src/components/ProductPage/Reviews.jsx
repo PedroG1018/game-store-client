69f52350b290186abc0a69f46a1b11a2d5ee4ad4
Pedro Gutierrez Jr <gutierrezpedro2112@gmail.com>
2023-04-26 11:11:05 -0500

    able to display product reviews

--- src/components/ProductPage/Reviews.jsx
+++ src/components/ProductPage/Reviews.jsx
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from "react";
 import Review from "./Review";
 import { collection, getDoc, query } from "firebase/firestore";
 import { db } from "../../firebase";
+import { Typography } from "@mui/material";
 
 const Reviews = ({ reviews }) => {
-  const [users, setUsers] = useState([]);
-
   return (
     <div>
+      <h1 className="text-blue-700 font-bold text-3xl">Reviews</h1>
       {reviews.map((review) => {
         return (
           <div key={review.id}>
