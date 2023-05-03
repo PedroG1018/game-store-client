8a7e8e9f03942ce89f6079009bafc2aba0dcccaa
Pedro Gutierrez Jr <gutierrezpedro2112@gmail.com>
2023-05-03 11:08:57 -0500

    cart items now displaying correctly

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -13,6 +13,7 @@ import { db } from "../firebase";
 
 const Cart = () => {
   const [cartItems, setCartItems] = useState([]);
+  const [quantities, setQuantities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const { currentUser } = useContext(AuthContext);
 
@@ -23,48 +24,48 @@ const Cart = () => {
 
   useEffect(() => {
     const fetchCart = async () => {
-      setIsLoading(true);
-
       await getDocs(cartQuery)
         .then(async (response) => {
+          const cartId = response.docs[0].id;
+
           const cartItemsQuery = query(
             collection(db, "cartItems"),
-            where("cartId", "==", response.docs[0].id)
+            where("cartId", "==", cartId)
           );
 
           await getDocs(cartItemsQuery)
             .then(async (response) => {
+              const items = response.docs;
               let products = [];
-              response.docs.forEach(async (item) => {
+              let qty = [];
+
+              for (const item of items) {
+                qty.push(item.data().quantity);
+
                 const docRef = doc(db, "products", item.data().productId);
+
                 await getDoc(docRef)
                   .then((response) => {
-                    products.push(response.data());
+                    products.push(response);
                   })
                   .catch((error) => {
-                    setIsLoading(false);
-                    console.log("Unable to fetch product:", error);
+                    console.log("Unable to fetch products:", error);
                   });
-              });
+              }
+
               setCartItems(products);
-              console.log(cartItems);
+              setQuantities(qty);
             })
             .catch((error) => {
-              setIsLoading(false);
               console.log("Unable to fetch cart items:", error);
             });
         })
         .catch((error) => {
-          setIsLoading(false);
           console.log("Unable to fetch cart:", error);
         });
     };
 
-    if (cartItems.length > 0) {
-      return;
-    }
     fetchCart();
-    setIsLoading(false);
   }, []);
 
   if (isLoading) {
@@ -78,42 +79,51 @@ const Cart = () => {
       </Typography>
       <div className="md:w-[36em] w-[30em] justify-center mx-auto">
         <hr />
-        {cartItems.map((cartItem) => (
-          <div className="flex justify-between my-6" key={cartItem.id}>
-            <div className="flex space-x-2">
-              <img
-                src="https://tailwindui.com/img/ecommerce-images/checkout-page-03-product-04.jpg"
-                alt="shirt"
-                className="w-[140px]"
-              />
-              <div>
-                <Typography>Hey</Typography>
-                <Typography>Hey</Typography>
-                <Typography>Hey</Typography>
-
-                <div className="flex space-x-2 mt-4">
-                  <select
-                    label="Qty"
-                    className="w-[4em] h-[2em] border border-gray-400 rounded-lg"
-                  >
-                    <option className="">1</option>
-                    <option selected="selected">2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                  </select>
-                  <Typography
-                    className="text-red-500 font-medium mt-2 cursor-pointer hover:font-semibold my-auto"
-                    onClick={() => alert("hey")}
-                  >
-                    Remove
+        {cartItems.map((cartItem, index) => {
+          return (
+            <div className="flex justify-between my-6" key={cartItem.id}>
+              <div className="flex space-x-2">
+                <img
+                  src={cartItem.data().image}
+                  alt="shirt"
+                  className="w-[140px]"
+                />
+                <div>
+                  <Typography>{cartItem.data().name}</Typography>
+                  <Typography className="capitalize">
+                    {cartItem.data().condition}
                   </Typography>
+                  {cartItem.data().quantity === 0 ? (
+                    <Typography>Out of Stock</Typography>
+                  ) : (
+                    <Typography>In Stock</Typography>
+                  )}
+
+                  <div className="flex space-x-2 mt-4">
+                    <select
+                      label="Qty"
+                      className="w-[4em] h-[2em] border border-gray-400 rounded-lg"
+                      defaultValue={quantities[index]}
+                    >
+                      <option className="">1</option>
+                      <option>2</option>
+                      <option>3</option>
+                      <option>4</option>
+                      <option>5</option>
+                    </select>
+                    <Typography
+                      className="text-red-500 font-medium mt-2 cursor-pointer hover:font-semibold my-auto"
+                      onClick={() => alert("hey")}
+                    >
+                      Remove
+                    </Typography>
+                  </div>
                 </div>
               </div>
+              <Typography>${cartItem.data().price}</Typography>
             </div>
-            <Typography>$hey</Typography>
-          </div>
-        ))}
+          );
+        })}
       </div>
       <div className="md:w-[36em] w-[30em] justify-center items-center mx-auto mt-6">
         <div className="flex justify-between mb-6">
